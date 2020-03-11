2546435e7e07b330d3b387449c8dc4e1c6e34924
Michał Listek <michal.listex@gmail.com>
2020-03-11 16:38:56 +0100

    Added sql connentor

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Connection connection = DriverManager
     }
 }
