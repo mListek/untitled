2956cbc943ec697850b76f1d36156476f87b2ff9
Michał Listek <michal.listex@gmail.com>
2020-03-11 20:06:20 +0100

    Working database connection

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,17 @@ package com.company;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) {
-        Connection connection = DriverManager
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/testing?useSSL=false&characterEncoding=utf8",
+                "user", "1369")) {
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
