823676c99674939b94194156cd638df759510d0e
Michał Listek <michal.listex@gmail.com>
2020-03-12 12:31:46 +0100

    Practicing creating and droping tables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,30 @@ package com.company;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        String sql = "CREATE TABLE users (user_id INT AUTO_INCREMENT,"
+                + " user_name VARCHAR(255),"
+                + " user_email VARCHAR(255) UNIQUE, "
+                + " PRIMARY KEY(user_id))";
+        String dropTable = "DROP TABLE users";
+
+        Scanner scan = new Scanner(System.in);
+
         try (Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/testing?useSSL=false&characterEncoding=utf8",
                 "user", "1369")) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+            scan.nextLine();
+            statement.executeUpdate(dropTable);
+
+
 
         } catch (SQLException e) {
             e.printStackTrace();
